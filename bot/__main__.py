a2918734471c19d44ab770220e4674be75986a3b
agrikol <agrikol1995@yandex.ru>
2024-08-01 13:13:21 +0700

    Start handler init

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,13 +1,17 @@
 import asyncio, logging
-from environs import Env
 from aiogram import Bot, Dispatcher
 from handlers import user_handlers
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from config.config_reader import config
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
-    bot: Bot = Bot(token=config.bot_token.get_secret_value())
+    bot: Bot = Bot(
+        token=config.bot_token.get_secret_value(),
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
     dp: Dispatcher = Dispatcher()
     dp.include_router(user_handlers.router)
     await bot.delete_webhook(drop_pending_updates=True)
