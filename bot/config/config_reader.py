86026e40f09aad0443d16381157d61b52451f6a4
agrikol <agrikol1995@yandex.ru>
2024-08-15 14:02:53 +0700

    Minor fix

--- bot/config/config_reader.py
+++ bot/config/config_reader.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     bot_token: SecretStr
 
     class Config:
-        env_file = os.path.expanduser("~/.env")
+        env_file = os.path.expanduser("/.env")
         env_file_encoding = "utf-8"
         env_nested_delimiter = "__"
 
