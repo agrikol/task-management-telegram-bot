06bdc589cdd63e7273cb33e36d367ba20e8ccec4
agrikol <agrikol1995@yandex.ru>
2024-08-27 18:33:06 +0700

    Db config added

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,18 +3,25 @@ from aiogram import Bot, Dispatcher
 from bot.handlers import commands
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from bot.config.config_reader import Settings
+from bot.config.config_reader import config
 from bot.dialogs.start.dialogs import start_dialog
 from bot.dialogs.create_task.dialogs import create_task_dialog
 from aiogram.fsm.storage.redis import RedisStorage
 from aiogram_dialog import setup_dialogs
 from aiogram.fsm.storage.base import DefaultKeyBuilder
 from redis.asyncio import Redis
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import text
 
 
 async def main():
-    config = Settings()
+
     logging.basicConfig(level=logging.INFO)
+
+    engine = create_async_engine(url=str(config.db_dsn), echo=config.is_echo)
+    async with engine.begin() as conn:
+        await conn.execute(text("SELECT 1"))
+
     bot: Bot = Bot(
         token=config.bot_token.get_secret_value(),
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
--- bot/config/config_reader.py
+++ bot/config/config_reader.py
@@ -1,13 +1,18 @@
 import os
-from pydantic import SecretStr, RedisDsn
+from pydantic import SecretStr, RedisDsn, PostgresDsn, BaseModel
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     bot_token: SecretStr
     redis_dsn: RedisDsn
+    db_dsn: PostgresDsn
+    is_echo: bool = True
 
     class Config:
         env_file = os.path.expanduser(".env")
         env_file_encoding = "utf-8"
         env_nested_delimiter = "__"
+
+
+config = Settings()
